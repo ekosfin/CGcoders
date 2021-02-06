41765d998ae5eda149392aac47e80cfcd79adabe
Markusstr <markus.strandman@gmail.com>
2021-02-06 15:06:55 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar, Nav, Button } from "react-bootstrap";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar className="navbar-container" collapseOnSelect expand="md" variant="dark">
+                <Navbar.Brand className="navbar-logo-container">Logo</Navbar.Brand>
+                <Navbar.Toggle className="navbar-toggle" aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="navbar-button-container">
+                        <Button variant="outline-light">Kirjaudu ulos</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
 export default App;
